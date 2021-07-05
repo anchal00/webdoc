9958d80dad7fceacf189e6fc6c73ec4551afae7f
anchal00 <anchal82199@gmail.com>
2021-07-06 00:14:48 +0530

    Implemented service (create task method) and utility classes

--- src/main/java/com/example/webdoc/service/TaskServiceImpl.java
+++ src/main/java/com/example/webdoc/service/TaskServiceImpl.java
@@ -9,21 +9,34 @@ import java.util.Set;
 
 import com.example.webdoc.model.Status;
 import com.example.webdoc.model.Task;
+import com.example.webdoc.util.ScheduledTask;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TaskServiceImpl implements TaskService{
+public class TaskServiceImpl implements TaskService {
 
+    Map<Task, Status> taskStore = new HashMap<>();
 
     @Autowired
     private ThreadPoolTaskScheduler scheduler;
-    
+
     @Override
     public boolean createTaskForUser(Task task) {
-        return false;
+        Status status = new Status();
+        status.setTaskName(task.getTaskName());
+        status.setUp(false);
+        status.setTimeElapsed(1);
+
+        task.setStatus(status);
+        taskStore.put(task, status);
+
+        int frequency = task.getFrequency();
+
+        scheduler.scheduleWithFixedDelay(new ScheduledTask(task), frequency);
+        return true;
     }
 
     @Override
---
+++ src/main/java/com/example/webdoc/util/ScheduledTask.java
@@ -0,0 +1,41 @@
+package com.example.webdoc.util;
+
+import java.net.http.HttpClient;
+import java.time.LocalDateTime;
+import java.util.concurrent.Callable;
+
+import com.example.webdoc.model.Task;
+
+public class ScheduledTask implements Runnable {
+
+    private Task task;
+
+    public ScheduledTask(Task task ){
+        this.task = task;
+
+    }
+
+    @Override
+    public void run() {
+       
+        boolean isUp = isWebsiteUp();
+        System.out.println("DEBUG#1 ::: Website -- "+isUp);
+        System.out.println("task "+ task.getTaskName() +" ran at - "+ LocalDateTime.now());
+        if (task.getStatus().isUp() == isUp) {
+            int i = task.getStatus().getTimeElapsed();
+            i++;
+            task.getStatus().setTimeElapsed(i);
+        } else {
+            task.getStatus().setUp(isUp);
+            task.getStatus().setTimeElapsed(1);
+        }
+    }
+
+
+    private boolean isWebsiteUp(){
+        int p = (int)(Math.random() * 100);
+        System.out.println("Random ==== "+ p);
+        return (p % 2 == 0)?true: false;
+    }
+    
+}
