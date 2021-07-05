32de76a91ecfae9a104d0af42418f8dbf58e4276
anchal00 <anchal82199@gmail.com>
2021-07-05 23:45:37 +0530

    Initialized project

---
+++ src/main/java/com/example/webdoc/WebdocApplication.java
@@ -0,0 +1,13 @@
+package com.example.webdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdocApplication.class, args);
+	}
+
+}
